02a0a305e7526a2ec0b5967f134318caeac68bb3
cyrilsl <lucascyrilsamuel@gmail.com>
2025-12-23 02:43:20 +0530

    auth type errors fixed..

--- components/auth/AuthModal.tsx
+++ components/auth/AuthModal.tsx
@@ -278,7 +278,7 @@ export function AuthModal({ isOpen, onClose }: AuthModalProps) {
                                 {otp.map((digit, index) => (
                                     <input
                                         key={index}
-                                        ref={(el) => (inputRefs.current[index] = el)}
+                                        ref={(el) => { inputRefs.current[index] = el; }}
                                         type="text"
                                         inputMode="numeric"
                                         maxLength={1}
